ce2c4a5be9b9cbd56ad24c1b8ae160f1f23514b5
Kerim yıldız <abdulkerim.yildiz@smartict.com.tr>
2019-03-14 23:03:16 +0300

    first commit

---
+++ Assets/Codes/StartupHandler.cs
@@ -0,0 +1,25 @@
+﻿using LwC;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartupHandler : MonoBehaviour
+{
+    public static List<Word> Words = new List<Word>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        Words.Add(new Word() { english = "car", turkish = "araba", level = 1, isUsed = false });
+        Words.Add(new Word() { english = "apple", turkish = "elma", level = 1, isUsed = false });
+        Words.Add(new Word() { english = "computer", turkish = "bilgisayar", level = 1, isUsed = false });
+        Words.Add(new Word() { english = "pen", turkish = "kalem", level = 1, isUsed = false });
+        Words.Add(new Word() { english = "paper", turkish = "kağıt", level = 1, isUsed = false });
+        Words.Add(new Word() { english = "ruler", turkish = "silgi", level = 1, isUsed = false });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Codes/Word.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LwC
+{
+    public class Word
+    {
+        public string turkish { get; set; }
+        public string english { get; set; }
+        public int id { get; set; }
+        public bool isUsed { get; set; }
+        public int level { get; set; }
+    }
+}
